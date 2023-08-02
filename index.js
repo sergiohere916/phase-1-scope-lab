5cd152d4709675ab5c72fca941b1af935b0449c0
Sergio Heredia <sergioheredia1996@gmail.com>
2023-08-02 16:50:21 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const LeastFavoriteCustomer = "Sergio";
+
+function upperCaseCustomerName() {
+    customerName = "BOB";
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "Sam";
+}
+
+
